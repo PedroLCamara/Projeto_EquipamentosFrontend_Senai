8fd321b7fe3817e1a19ccfff21ca06e7e7015a6c
PedroLCamara <plcrlschool@gmail.com>
2022-02-15 22:00:35 -0300

    TesteIntegracaoCadastroExclusao

--- cypress/integration/E2ETests/cadastrarexcluirequipamentos.test.js
+++ cypress/integration/E2ETests/cadastrarexcluirequipamentos.test.js
@@ -1,25 +1,32 @@
-describe('Cadastro e exclusão de equipamentos', ()=>{
-    beforeEach(()=>{
-        cy.visit('http://localhost:300')
+describe('Cadastro e exclusão de equipamentos', () => {
+    beforeEach(() => {
+        cy.visit('http://localhost:3000')
+    })
 
-        it('Deve logar, acessar a página de equipamentos, cadastrar um novo equipamento e excluí-lo', ()=>{
-            cy.get('.cabecalhoPrincipal-nav-login').should('exist')
-            cy.get('.cabecalhoPrincipal-nav-login').click()
-            cy.get('.input__login').first().type('paulo@email.com')
-            cy.get('.input__login').last().type('123456789')
-            cy.get('.btn__login').click()
+    it('Deve logar, acessar a página de equipamentos, cadastrar um novo equipamento e excluí-lo', () => {
+        cy.get('.cabecalhoPrincipal-nav-login').should('exist')
+        cy.get('.cabecalhoPrincipal-nav-login').click()
+        cy.get('.input__login').first().type('paulo@email.com')
+        cy.get('.input__login').last().type('123456789')
+        cy.get('.btn__login').click()
 
-            cy.get('input[type=file]').first().selectFile('src/assets/css/img/tests/equipamento.jpeg')
+        cy.wait(3000)
 
-            cy.wait(3000)
+        cy.get('input[type=file]').first().selectFile('src/assets/tests/equipamento.jpeg')
 
-            cy.get('input[type=file]').last().selectFile('src/assets/css/img/tests/equipamento.jpeg')
-            cy.get("input[type=text]").first().type('Equipamento teste')
-            cy.get(".btn__cadastro").click()
+        cy.wait(3000)
 
-            cy.wait(3000)
-
-            cy.get('.card div span').first().should('have.text', 'Patrimônio: ' )
+        cy.get('input[type=file]').last().selectFile('src/assets/tests/equipamento.jpeg')
+        cy.get("input[type=text]").first().type('Equipamento teste cypress')
+        let valorOcr;
+        cy.get('#codigoPatrimonio').then(($input) => {
+            valorOcr = $input.text()
         })
+        cy.get(".btn__cadastro").click()
+
+
+        cy.wait(3000)
+
+        cy.get('.card').filter(':contains("Equipamento teste cypress")').children('.excluir').click()
     })
 })
